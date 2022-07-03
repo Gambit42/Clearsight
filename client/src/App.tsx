6e2a5584a480e7c79ecd8569983db414193f9402
Prins Ocampo <ocampoprins@gmail.com>
2022-07-03 15:16:34 +0800

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./tailwind.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500">Template</h1>
+    </div>
+  );
+}
+
+export default App;
